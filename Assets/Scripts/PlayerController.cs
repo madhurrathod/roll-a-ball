a85cb108cf8745b17faff977949c7f3df305c954
Madhur Rathod <madhur.thanku@gmail.com>
2025-07-26 22:38:59 +0530

    final hit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,65 @@
+using System.Threading;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject WinText;
+    private int count = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        settext();
+        WinText.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
+
+    void settext()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            WinText.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            settext();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            WinText.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+            WinText.SetActive(true);
+        }
+    }
+
 }
